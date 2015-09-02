af708a637eff7fc39f7be5b2b9ccd9dde05374f1
coffeephantom <jfyy00@126.com>
2015-09-02 23:45:07 +0800

    修改错误

--- route_assign.py
+++ route_assign.py
@@ -1,9 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, flash
 app = Flask(__name__)
 
-@app.route('/sum/<int:a>/<int:b>')
+@app.route('/sum/<int:a>/<int:b>/')
 def show_sum(a,b):
-    return a+b
+    return '%d' % a, '%d' %b 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
